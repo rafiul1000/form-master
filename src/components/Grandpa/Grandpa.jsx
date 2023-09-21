ea1dc2081daa1abac26ab0486478e5828f3ae5c3
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-21 12:09:28 +0600

    Prop Drilling to pass data to a many level components

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,34 @@
+import { createContext } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
-import './Grandpa.css'
+import "./Grandpa.css";
+
+const AssetContext = createContext("gold");
 
 const Grandpa = () => {
-    return (
-        <div className="grandpa div">
-            <h2>Grandpa</h2>
-            <section className="flex">
-            <Dad></Dad>
-            <Uncle></Uncle>
-            <Aunty></Aunty>
-            </section>
-        </div>
-    );
+  const asset = "diamond";
+
+  return (
+    <div className="grandpa div">
+      <h2>Grandpa</h2>
+      <AssetContext.Provider value="gold">
+        <section className="flex">
+          <Dad asset={asset}></Dad>
+          <Uncle asset={asset}></Uncle>
+          <Aunty></Aunty>
+        </section>
+      </AssetContext.Provider>
+    </div>
+  );
 };
 
 export default Grandpa;
+
+/* 
+
+1. create a context and export it
+
+2. add provider for the context with a value
+
+*/
